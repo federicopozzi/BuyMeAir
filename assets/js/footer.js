5ba5ed0f613a47655c3490629d3eba6c0d744402
Federico <federico.izzop@gmail.com>
2021-02-08 11:26:41 +0100

    fix

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -11,9 +11,7 @@ document.getElementById("footer").innerHTML = `
      <img src="../assets/svg/polimi_white.svg" class="img-fluid" alt="Responsive image" width="200">
 </div>
 <div class="col-lg-5 col-7">
-     <span class="textFoot">© All pictures shown at Itscheaperabroad belong to Instagram.
-Instagram is a registered trademark and it’s part of The Facebook Company.
-Images are not modified nor altered. You may download anything on these pages for your own personal, non-commercial use.</span>
+     <span class="textFoot">© the content of this site is only academic and does not represent reality</span>
 </div>
 </div>
 </div>
