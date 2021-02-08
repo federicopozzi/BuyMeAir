1947abf0453e0c7282fc32217293a19b23609ebd
Federico <federico.izzop@gmail.com>
2021-02-08 14:17:44 +0100

    about + footer

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,8 +1,9 @@
-document.getElementById("footer").innerHTML = `
 
-    <section class="websiteMargin pb-5 mt-3">
+      document.getElementById("footer").innerHTML = `
+
+<div class="displayDesk">
+<section class="websiteMargin pt-3 pb-5 mt-3">
 <div class="container-fluid">
-
 <div class="row">
      <div class="col-lg-3 col-2">
      <img src="../assets/svg/density_white.svg" class="img-fluid" alt="Responsive image" width="50">
@@ -11,10 +12,16 @@ document.getElementById("footer").innerHTML = `
      <img src="../assets/svg/polimi_white.svg" class="img-fluid" alt="Responsive image" width="200">
 </div>
 <div class="col-lg-5 col-7">
-     <span class="textFoot">© the content of this site is only academic and does not represent reality</span>
+     <span class="textFoot">© The content of this site is only academic and does not represent reality</span>
 </div>
 </div>
 </div>
 </section>
+</div>
+
+
+
 
       `
+
+
