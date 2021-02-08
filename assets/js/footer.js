a4762670a1cd669bd818641d00b66a8e4c1b3024
Federico <federico.izzop@gmail.com>
2021-02-08 11:18:54 +0100

    footer

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,7 +1,22 @@
 document.getElementById("footer").innerHTML = `
 
-      <div>
-PUT FOOTER HERE
-      </div>
+    <section class="websiteMargin pb-5 mt-3">
+<div class="container-fluid">
+
+<div class="row">
+     <div class="col-lg-3 col-2">
+     <img src="../assets/svg/density_white.svg" class="img-fluid" alt="Responsive image" width="50">
+</div>
+<div class="col-lg-4 col-3">
+     <img src="../assets/svg/polimi_white.svg" class="img-fluid" alt="Responsive image" width="200">
+</div>
+<div class="col-lg-5 col-7">
+     <span class="textFoot">© All pictures shown at Itscheaperabroad belong to Instagram.
+Instagram is a registered trademark and it’s part of The Facebook Company.
+Images are not modified nor altered. You may download anything on these pages for your own personal, non-commercial use.</span>
+</div>
+</div>
+</div>
+</section>
 
       `
